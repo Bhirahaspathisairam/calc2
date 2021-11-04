5970ee7d6b1a7450601b05ee0125befc3d9a02e7
Bhirahaspathi sairam <bs548@njit.edu>
2021-11-04 18:11:19 -0400

    editied files according to pylint

--- calc/calculation.py
+++ calc/calculation.py
@@ -1,13 +1,20 @@
 """This is our calculation base class / Abstract Class"""
+#pylint: disable=too-few-public-methods
 class Calculation:
+    """This is our calculation base class / Abstract Class"""
 
-    #contstructor and it is the first function called when an object of the class is instantiated
+    #contstructor and it is the first function
+    # called when an object of the class is instantiated
     def __init__(self,value_a, value_b):
         #self references the instantiated object of the class
-        #these are instance properties that are being sharred with the child classes (addition, subtraction, etc...)
+        #these are instance properties that are being
+        # sharred with the child classes (addition, subtraction, etc...)
+        """This is the Constructor for calculation class"""
         self.value_a = value_a
         self.value_b = value_b
-    # Class Factory Method <- bound to the class and not the instance of the class
+    # Class Factory Method <- bound to
+    # the class and not the instance of the class
     @classmethod
     def create(cls, value_a, value_b):
+        """This is the create Factory method"""
         return cls(value_a,value_b)
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -5,9 +5,14 @@ import pytest
 
 from calculator.calculator import Calculator
 
-#this is how you define a function that will run each time you pass it to a test, it is called a fixture
+#this is how you define a function
+# that will run each time you pass it
+# to a test, it is called a fixture
+#pylint: disable=unused-argument
+#pylint: disable=redefined-outer-name
 @pytest.fixture
 def clear_history():
+    """This fixture will clear the history"""
     Calculator.clear_history()
 
 def test_calculator_add(clear_history):
@@ -21,26 +26,30 @@ def test_calculator_add(clear_history):
     pprint.pprint(Calculator.history)
 
 def test_clear_history(clear_history):
+    """this Test function will check for empty history List"""
     assert Calculator.add_number(1,2) == 3
     assert Calculator.add_number(2, 2) == 4
     assert Calculator.add_number(3, 2) == 5
     assert Calculator.add_number(4, 2) == 6
     assert Calculator.history_count() == 4
-    assert Calculator.clear_history() == True
+    assert Calculator.clear_history() is True
     assert Calculator.history_count() == 0
 
 def test_count_history(clear_history):
+    """This test will check for count of the history"""
     assert Calculator.history_count() == 0
     assert Calculator.add_number(2, 2) == 4
     assert Calculator.add_number(3, 2) == 5
     assert Calculator.history_count() == 2
 
 def test_get_last_calculation_result(clear_history):
+    """This Test the last calculated Result value"""
     assert Calculator.add_number(2, 2) == 4
     assert Calculator.add_number(3, 2) == 5
     assert Calculator.get_result_of_last_calculation_added_to_history() == 5
 
 def test_get_first_calculation_result(clear_history):
+    """This Test the last calculated Result value"""
     assert Calculator.add_number(2, 2) == 4
     assert Calculator.add_number(3, 2) == 5
     assert Calculator.get_result_of_first_calculation_added_to_history() == 4
@@ -52,3 +61,8 @@ def test_calculator_subtract(clear_history):
 def test_calculator_multiply(clear_history):
     """ tests multiplication of two numbers"""
     assert Calculator.multiply_numbers(1,2) == 2
+
+def test_calculator_divide(clear_history):
+    """tests division of two numbers"""
+    assert Calculator.divide_numbers(2,1) == 2
+    assert Calculator.divide_numbers(2,0) == 0
