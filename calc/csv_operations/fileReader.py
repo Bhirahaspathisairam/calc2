de3b79ecb36fc0debfa2bbf40d90ab6b10522351
Bhirahaspathi sairam <bs548@njit.edu>
2021-12-02 17:57:07 -0500

    Added input csv files

---
+++ calc/csv_operations/fileReader.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+class FileReader:
+
+    @staticmethod
+    def read_from_file(filename):
+        dir_path = os.getcwd()
+        print(dir_path)
+        # file_path = os.path.join(dir_path,"/data/", filename)
+        # df = pd.read_csv(file_path)
+        # return df
